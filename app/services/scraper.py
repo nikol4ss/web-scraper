b2e85d884a25e4c2453440304996f82d8d82dd52
Nikolas Campos <nikolassc4mpos@gmail.com>
2025-05-17 11:14:47 -0300

    feat(scraper): scrape_product
    function opens the browser, searches for a product on Mercado Livre, extracts the title and price with CSS selectors and saves it in a CSV with a timestamp.
    If the product is not found, it throws an error; otherwise, it adds or creates the corresponding CSV.

--- app/services/scraper.py
+++ app/services/scraper.py
@@ -1,1 +1,38 @@
-# Lógica Selenium
+import os
+import time
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def scrape_product(product: str):
+    driver = webdriver.Chrome()
+    search = f"https://www.mercadolivre.com.br/{product.replace(" ", "-")}"
+
+    driver.get(search)
+    time.sleep(3)
+
+    try:
+        title = driver.find_element(By.CSS_SELECTOR, '#header-octopus > div > div.ui-pdp-header__title-container > h1').text
+        price = driver.find_element(By.CSS_SELECTOR, '#price > div > div.ui-pdp-price__main-container > div.ui-pdp-price__second-line').text
+
+    except:
+        driver.quit()
+        raise Exception("Product not found")
+
+    df = pd.DataFrame([{
+        "product": product,
+        "title": title,
+        "price": float(price.replace("R$", "").replace(".", "").replace(",", ".").strip()),
+        "timestamp": pd.Timestamp.now()
+    }])
+
+    csv_path = f"data/{product}.csv"
+    os.makedirs("data", exist_ok=True)
+
+    if os.path.exists(csv_path):
+        df.to_csv(csv_path, mode="a", header=False, index=False)
+
+    else:
+        df.to_csv(csv_path, index=False)
+
+    driver.quit()
