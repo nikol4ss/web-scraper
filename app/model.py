8688d4e0188ea6d47fb5de64ac5a380ff8e56990
Nikolas Campos <nikolassc4mpos@gmail.com>
2025-05-17 10:30:15 -0300

    feat(models): PriceEntry, PriceResponse
    Creation of models using BaseModel, and typing done

--- app/model.py
+++ app/model.py
@@ -1,1 +1,14 @@
-# Schemas de resposta
+from typing import List
+from datetime import datetime
+from pydantic import BaseModel
+
+class PriceEntry(BaseModel):
+    product: str
+    title: str
+    price: float
+    timestamp: datetime
+
+class PriceResponse(BaseModel):
+    product: str
+    average_price: float
+    history: List[PriceEntry]
