7f72cd50d5f2c37abf8a10294880c3009fe23cc6
Nikolas Campos <nikolassc4mpos@gmail.com>
2025-05-19 15:12:31 -0300

    feat(fastAPI): created endpoint
    endpoint created and authenticated with fastapi in main.py

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,1 +1,15 @@
-# Endpoints da API
+from fastapi import APIRouter
+from app.scraper import scrape_product
+from app.handler import get_price_history
+from app.models import PriceResponse
+
+router = APIRouter()
+
+@router.get("/scrape/{product}")
+def scrape(product: str):
+    scrape_product(product)
+    return {"message": f"Scraping iniciado para '{product}'."}
+
+@router.get("/prices/{product}", response_model=PriceResponse)
+def prices(product: str):
+    return get_price_history(product)
--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# Inicia FastAPI
+from fastapi import FastAPI
+from app.routes import router
+
+app = FastAPI(title="Price Tracker API")
+app.include_router(router)
+
+
+uvicorn app.main:app --reload
